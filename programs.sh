493bff5b367fa972aaffb0c278ab54bb54139e56
Felipe Leal <lealfelipealves@gmail.com>
2021-10-02 12:01:37 -0300

    feat: basic programs

---
+++ programs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+echo -e '\e[7;103;30m ======================== \e[0m'
+echo -e '\e[7;103;30m   Pacote de instalação   \e[0m'
+echo -e '\e[7;103;30m ======================== \e[0m'
+
+#sudo apt install -y software-properties-common
+echo -e '\e[0;49;92m> \e[1;49;36msoftware-properties-common\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo apt install -y curl
+echo -e '\e[0;49;92m> \e[1;49;36mcURL\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo apt install -y xclip
+echo -e '\e[0;49;92m> \e[1;49;36mxclip\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo apt install -y build-essential
+echo -e '\e[0;49;92m> \e[1;49;36mbuild-essential\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo apt install -y libssl-dev
+echo -e '\e[0;49;92m> \e[1;49;36mlibssl-dev\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo apt install -y net-tools
+echo -e '\e[0;49;92m> \e[1;49;36mnet-tools\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo apt install -y htop
+echo -e '\e[0;49;92m> \e[1;49;36mhtop\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo apt install -y snapd
+echo -e '\e[0;49;92m> \e[1;49;36msnapd\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo snap install dbeaver-ce
+echo -e '\e[0;49;92m> \e[1;49;36mdbeaver-ce\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo snap install insomnia
+echo -e '\e[0;49;92m> \e[1;49;36minsomnia\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo snap install postman
+echo -e '\e[0;49;92m> \e[1;49;36mpostman\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo snap install code --classic
+echo -e '\e[0;49;92m> \e[1;49;36mvisual code\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo snap install slack --classic
+echo -e '\e[0;49;92m> \e[1;49;36mslack\e[0;49;92m instalado com sucesso.\e[0m'
+
+#sudo snap install discord
+echo -e '\e[0;49;92m> \e[1;49;36mdiscord\e[0;49;92m instalado com sucesso.\e[0m'
